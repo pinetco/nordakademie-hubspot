605267bc77f428aedcdf6dc1c9d1aa4e430f024f
jaypinetco <jay@pinetco.com>
2022-05-30 11:14:34 +0530

    Initial setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,10 +2,10 @@ let mix = require('laravel-mix');
 
 const tailwindcss = require('tailwindcss');
 
-mix.js('src/js/vendors.js', 'public/assets/js')
-    .js('src/js/app.js', 'public/assets/js')
-    .sass('src/scss/vendors.scss', 'public/assets/css')
-    .sass('src/scss/app.scss', 'public/assets/css')
+mix.js('src/js/vendors.js', 'nordakademie-hubspot/assets/js')
+    .js('src/js/app.js', 'nordakademie-hubspot/assets/js')
+    .sass('src/scss/vendors.scss', 'nordakademie-hubspot/assets/css')
+    .sass('src/scss/app.scss', 'nordakademie-hubspot/assets/css')
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.config.js') ],

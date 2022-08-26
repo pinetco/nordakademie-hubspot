cdcbf2b4c154ecd3bb9ee33317f2dccabe59fe77
jaypinetco <jay@pinetco.com>
2022-08-26 11:19:35 +0530

    changes js and css

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,10 @@
-/* Smooth Scroll Logic */
+// Smooth Scroll Logic //
 $("[data-scroll='true']").click(function () {
     event.preventDefault();
-
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
-
 // One Col Slider Logic
 $('.one-col-slider').slick({
     dots: false,
@@ -18,12 +16,27 @@ $('.one-col-slider').slick({
     fade: true,
     cssEase: 'linear',
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 5000
 });
-
+// Two Col Slider Logic
+$('.two-col-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [{
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+            dots: true
+        }
+    }]
+});
 // Three Col Slider Logic
 $('.three-col-slider').slick({
-    dots: true,
+    dots: false,
     arrows: false,
     autoplaySpeed: 5000,
     infinite: true,
@@ -32,58 +45,74 @@ $('.three-col-slider').slick({
     slidesToScroll: 1,
     focusOnSelect: false,
     swipeToSlide: true,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 640,
-            settings: {
-                slidesToShow: 1,
-            }
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2
         }
-    ]
+    }, {
+        breakpoint: 640,
+        settings: {
+            slidesToShow: 1,
+            dots: true
+        }
+    }]
+});
+// icon and text repeater Slider Logic
+//   function mobileOnlySlider() {
+$('.icon-text-repeater').slick({
+    dots: false,
+    arrows: false,
+    autoplaySpeed: 5000,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: false,
+    swipeToSlide: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2
+        }
+    }, {
+        breakpoint: 640,
+        settings: {
+            slidesToShow: 1,
+            dots: true,
+            arrows: true
+        }
+    }]
 });
 // Five Col Slider Logic
+
 $('.five-col-slider').slick({
     dots: true,
-    arrows: false,
+    arrows: true,
     infinite: false,
     speed: 300,
     slidesToShow: 5,
     slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 4,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 2,
-            }
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3
         }
-    ]
+    }, {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            arrows: false
+        }
+    }, {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2
+        }
+    }]
 });
-
 // Six Col Slider Logic
+
 $('.six-col-slider').slick({
     dots: true,
     arrows: false,
@@ -93,44 +122,117 @@ $('.six-col-slider').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
     swipeToSlide: true,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 4,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-            }
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 4
         }
-    ]
+    }, {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 3
+        }
+    }, {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2
+        }
+    }]
 });
-
 /* Tab Logic */
-$('.tab-link').click( function() {
+
+$('.tab-link').click(function () {
     var tabID = $(this).attr('data-tab');
     $(this).addClass('active').siblings().removeClass('active');
-    $('#tab-'+tabID).addClass('active').siblings().removeClass('active');
+    $('#tab-' + tabID).addClass('active').siblings().removeClass('active');
 });
-
 /*  FAQ Logic */
-$(".accordion-question").click(function(){
+
+$(".accordion-question").click(function () {
     var isActive = $(this).parent().hasClass("is-active");
-    if(isActive){
+
+    if (isActive) {
         return;
-    }else{
+    } else {
         $(this).parent().siblings().removeClass("is-active");
         $(this).parent().addClass("is-active");
         $(this).parent().siblings().find(".accordion-answer").slideUp();
         $(this).next().slideDown();
     }
+});
+//study-program card slider
+
+function mobileOnlySlider() {
+    $('.study-card').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: false,
+        autoplaySpeed: 5000
+    });
+    $('.study-card').slick('slickRemove', -1);
+}
+
+if (window.innerWidth < 768) {
+    mobileOnlySlider();
+}
+
+$(window).resize(function (e) {
+    if (window.innerWidth < 768) {
+        if (!$('.study-card').hasClass('slick-initialized')) {
+            mobileOnlySlider();
+        }
+    } else {
+        if ($('.study-card').hasClass('slick-initialized')) {
+            $('.study-card').slick('unslick');
+        }
+    }
+}); // quotation-slider
+
+$('.quotation-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    cssEase: 'linear',
+    autoplay: false,
+    autoplaySpeed: 5000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            arrows: false
+        }
+    }]
+}); // Three Col Slider Logic
+
+$('.counter-slider').slick({
+    dots: false,
+    arrows: false,
+    autoplaySpeed: 5000,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: false,
+    swipeToSlide: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2
+        }
+    }, {
+        breakpoint: 640,
+        settings: {
+            slidesToShow: 1,
+            dots: true,
+            arrows: true
+        }
+    }]
 });
